a9cb0632e28370724bd66071c8e6d7eca344620f
ugandar <dasariugandar42@gmail.com>
2017-01-05 07:27:31 +0000

    Removed the path module

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var server = require('express');
 var app = server();
 var moment = require('moment');
 var fs = require('fs');
-var path = require('path');
+//var path = require('path');
 
 var port = process.env.PORT || 8080;
 
@@ -10,38 +10,40 @@ app.listen(port, function(){
   console.log("Listening on port: " + port);
 });
 
+// we can eliminate this line by using the oath module and asingning the directory in the routes 
+app.use(server.static(__dirname+'/src'));
+
 app.get('/', function(req, res) {
-  var fileName = path.join(__dirname, 'index.html');
-  res.sendFile(fileName, function (err) {
+  
+  res.sendFile('index.html',function (err) {
     if (err) {
       console.log(err);
       res.status(err.status).end();
     }
     else {
-      console.log('Sent:', fileName);
+      res.sendFile('index.html');
     }
   });
 });
 
 app.get('/:datestring', function(req,res) {
   var myDate;
-  if(/^\d{8,}$/.test(req.params.datestring)) {
+  if(/^\d/.test(req.params.datestring)) {
     myDate = moment(req.params.datestring, "X");
   } else {
     myDate = moment(req.params.datestring, "MMMM D, YYYY");
   }
-
+  
   if(myDate.isValid()) {
     res.json({
       unix: myDate.format("X"),
       natural: myDate.format("MMMM D, YYYY")
     });
-  } else {
+  } 
+  else {
     res.json({
       unix: null,
       natural: null
     });
   }
-
-
 });
